5a5a066b7b2399b3039924f406c934fd5a40be9a
FreeJ99 <jenkoslobodan@gmail.com>
2022-05-06 12:06:46 +0200

    Created getUsers and getMeetings route. Added mongoDB support.

--- server/src/controllers/MeetingsController.js
+++ server/src/controllers/MeetingsController.js
@@ -2,19 +2,23 @@ const util = require('util');
 const MongoDBService = require('../services/MongoDBService');
 const ObjectId = require('mongodb').ObjectID;
 
+const DB_URL = 'mongodb+srv://sjenko:FBcqFYlFwIot7Ec3@cluster0.djdm8.mongodb.net/ReactCalendar?retryWrites=true&w=majority'
 
 class MeetingsController {
   constructor(request, response) {
     this.request = request;
     this.response = response;
 
-    this.mongoDBService = new MongoDBService('mongodb://root:example@localhost:27017',
-      'matf_lecture_08_example_03');
+    this.mongoDBService = new MongoDBService(DB_URL, 'ReactCalendar');
   }
 
   static registerRoutes(app) {
     app.get('/meetings', (request, response) => {
       new MeetingsController(request, response).getMeetings();
+    });
+
+    app.get('/users', (request, response) => {
+      new MeetingsController(request, response).getUsers();
     });
   }
 
@@ -24,9 +28,22 @@ class MeetingsController {
     this.response.send(ret);
   }
 
+  async getUsers() {
+    await this.mongoDBService.connect();
+
+    let users = await this.mongoDBService.find('users');
+
+    this.mongoDBService.disconnect();
+    this.response.send(users);
+  }
+
   async getMeetings() {
-    // await this.mongoDBService.connect();
-    this.response.send("Bla bla bla");
+    await this.mongoDBService.connect();
+
+    let meetings = await this.mongoDBService.find('meetings');
+
+    this.mongoDBService.disconnect();
+    this.response.send(meetings);
   }
 
   
--- server/src/services/MongoDBService.js
+++ server/src/services/MongoDBService.js
@@ -27,46 +27,6 @@ class MongoDBService {
       });
     });
   }
-
-  findOne(collection, parameters) {
-    return new Promise((resolve, reject) => {
-      this.database.collection(collection).findOne(parameters, function (error, data) {
-        if (error) reject();
-
-        resolve(data);
-      });
-    });
-  }
-
-  insert(collection, parameters) {
-    return new Promise((resolve, reject) => {
-      this.database.collection(collection).insertOne(parameters, function (error) {
-        if (error) reject();
-
-        resolve();
-      });
-    });
-  }
-
-  update(collection, findParameters, updateParameters) {
-    return new Promise((resolve, reject) => {
-      this.database.collection(collection).updateOne(findParameters, { $set: updateParameters }, function (error) {
-        if (error) reject();
-
-        resolve();
-      });
-    });
-  }
-
-  delete(collection, findParameters) {
-    return new Promise((resolve, reject) => {
-      this.database.collection(collection).deleteOne(findParameters, function (error) {
-        if (error) reject(error);
-
-        resolve();
-      });
-    });
-  }
 }
 
 module.exports = MongoDBService;
